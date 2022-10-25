9e98aac644a89c3653382470af42a83a7ddd005e
John G <johngouldman@pm.me>
2022-10-24 21:54:22 -0500

    part 1

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+
+import NavBar from "../NavBar"
+
+
+const AboutPage = () => {
+    return (
+        <div>
+            <NavBar />
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non metus ut ligula iaculis placerat eget at eros. Donec id dolor facilisis, euismod lectus ac, tincidunt ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed ante ante, laoreet vel finibus in, vestibulum sit amet mi. In lobortis pretium placerat. Praesent pulvinar arcu at odio vulputate lacinia. Fusce tempor ultricies sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tincidunt vitae sapien vel blandit. Duis est libero, egestas et elementum at, lobortis non justo. Morbi ac risus pellentesque, viverra elit quis, commodo felis. Pellentesque pharetra vel diam vitae dictum. Etiam at sagittis erat. Nunc sit amet vehicula nulla.</p>
+        </div>
+    )
+}
+
+export default AboutPage
---
+++ src/pages/Contact.js
@@ -0,0 +1,37 @@
+import NavBar from "../NavBar"
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+
+const ContactPage = () => {
+    const navigate = useNavigate()
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    return (
+        <div>
+            <NavBar />
+            <h1>Contact</h1>
+<label>First Name: </label>
+<input type='text' onChange={(e)=>{
+    console.log('Before', firstName);
+    setFirstName(e.target.value)
+    console.log('After', firstName);
+}}/>
+<br/>
+<label>Last Name: </label>
+<input type='text' onChange={(e)=>{
+setLastName(e.target.value)
+}}/>
+<br/>
+<label>Email: </label>
+<input type='text' onChange={(e)=>{
+    setEmail(e.target.value)
+}}/>
+<button onClick={()=>{
+    navigate("/")
+}}>Submit</button>
+        </div>
+    )
+}
+
+export default ContactPage
